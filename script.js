75916ee6393bb6571e64f73991e971805f39348a
rtocastro <ricktorres@live.com>
2023-10-23 23:32:02 -0700

    ADDED COMMENTS ON FUNCTIONALITY OF JSCRIPT

--- script.js
+++ script.js
@@ -5,18 +5,7 @@
 
 $(function () {
 
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
-  //HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  //Added functionality to Save button to return key/value and retrieve the ID and text in the area. 
+  //ADDED FUNCTIONALITY TO SAVE BUTTON TO RETURN KEY/VALUE AND RETRIEVE THE ID/TEXT AREA INFO.
 
   $(".saveBtn").on('click', function () {
     var key = $(this).parent().attr('id')
@@ -25,20 +14,15 @@ $(function () {
     localStorage.setItem(key, value)
   })
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
   //Displays current time.
-
-
   var currentTime = dayjs().hour()
+
+  //RUNS A LOOP ON TIMEBLOCK CLASS AND PARSES THAT AIMS FOR THE ID SPLITS IT AND MAKES IT ARRAY SO IT'LL BE EASIER TO COMPARE TIME TO NUMBER
   $('.time-block').each(function () {
     var timeBlockHour = parseInt($(this).attr('id').split('-')[1])
    
+//IF STATEMENT THAT GIVES STYLE/ADDS CLASS TO TIMEBLOCKS AND GIVE COLOR GREY, RED OR GREEN DEPENDING ON TIME.
+
   if (timeBlockHour < currentTime) {
 
     $(this).addClass("past")
@@ -51,9 +35,8 @@ $(function () {
 
   })
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // GETS LOCAL STORAGE OF TEXTAREA IN HOURS 
+
   $("#hour-9 textarea").val(localStorage.getItem("hour-9"))
   $("#hour-10 textarea").val(localStorage.getItem("hour-10"))
   $("#hour-11 textarea").val(localStorage.getItem("hour-11"))
